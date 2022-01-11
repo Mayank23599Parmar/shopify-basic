ccdcc27ae94ec6e66389157daf6e16c0e629c4a2
mayank58 <mayank@lucentinnovation.co.in>
2022-01-11 21:48:31 +0530

    cart page issue solve

--- javascript/cart-page.js
+++ javascript/cart-page.js
@@ -1,4 +1,4 @@
-import { addToCart ,cartChange,clearCart} from "./helper";
+import { addToCart, cartChange, clearCart } from "./helper";
 class CartPage {
     constructor() {
         this.init()
@@ -6,53 +6,61 @@ class CartPage {
     cartInit = async () => {
         let res = await fetch('/cart?view=ajax');
         let resJson = await res.text();
-        document.getElementById("cart-idss").innerHTML = resJson
+        document.getElementById("cart-idss").innerHTML = resJson;
+        this.clickEvent();
     }
     clickEvent = () => {
         //add to cart quantitty
-        const that=this
-        let addCartBiutton = document.querySelector(".cart-plus");
-        let minusCartBiutton = document.querySelector(".cart-minus");
-        let emptyCart=document.querySelector(".empty-cart")
-        let variantId;
-        let quantity = 1;
-        let formData = {}
+        const that = this
+        let cartItems = document.querySelectorAll('.cart-items-wrapper .cart-item');
+        cartItems.forEach(item=>{
+            let addCartBiutton = item.querySelector('.cart-plus');
             addCartBiutton.addEventListener("click", (e) => {
+                debugger;
+                let variantId;
+                let quantity = 1;
+                let formData = {}
                 variantId = e.currentTarget.dataset.variant;
                 if (variantId) {
                     formData = {
                         id: variantId,
                         quantity: quantity
                     }
-                     addToCart(formData,this.cartInit);
+                    addToCart(formData, that.cartInit);
                 }
             })
+        })
+        let addCartBiutton = document.querySelectorAll(".cart-plus");
+        let minusCartBiutton = document.querySelector(".cart-minus");
+        let emptyCart = document.querySelector(".empty-cart")
         
+        
+
         // minus cart quantity
-        if(minusCartBiutton){
-            minusCartBiutton.addEventListener("click",(e)=>{
+        if (minusCartBiutton) {
+            minusCartBiutton.addEventListener("click", (e) => {
                 variantId = e.currentTarget.dataset.variant;
-                let variantQantity=e.currentTarget.dataset.quantity
+                let variantQantity = e.currentTarget.dataset.quantity
                 if (variantId && variantQantity) {
                     formData = {
                         id: variantId,
                         quantity: parseInt(variantQantity) - 1
                     }
-                    
-                    cartChange(formData,this.cartInit)
+
+                    cartChange(formData, this.cartInit)
                 }
             })
         }
         //empty cart
-         if(emptyCart){
-            emptyCart.addEventListener("click",()=>{
+        if (emptyCart) {
+            emptyCart.addEventListener("click", () => {
                 clearCart(window.location.reload())
             })
-         }
+        }
     }
     init = () => {
         this.clickEvent();
-       
+
     }
 }
 
