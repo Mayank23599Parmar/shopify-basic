681e7d10fe999456b7fe9de8609285ab0b5dc551
mayank58 <mayank@lucentinnovation.co.in>
2022-01-12 01:29:49 +0530

    cart page footer about contact is done

--- javascript/cart-page.js
+++ javascript/cart-page.js
@@ -13,44 +13,46 @@ class CartPage {
         //add to cart quantitty
         const that = this
         let cartItems = document.querySelectorAll('.cart-items-wrapper .cart-item');
-        cartItems.forEach(item=>{
+        let variantId;
+        let quantity = 1;
+         
+        let formData = {}
+        cartItems.forEach(item => {
             let addCartBiutton = item.querySelector('.cart-plus');
-            addCartBiutton.addEventListener("click", (e) => {
-                debugger;
-                let variantId;
-                let quantity = 1;
-                let formData = {}
-                variantId = e.currentTarget.dataset.variant;
-                if (variantId) {
-                    formData = {
-                        id: variantId,
-                        quantity: quantity
+            let minusCartBiutton = item.querySelector(".cart-minus");
+            // plus cart quantity
+            if (addCartBiutton) {
+                addCartBiutton.addEventListener("click", (e) => {   
+                    variantId = e.currentTarget.dataset.variant;
+                    console.log(variantId,"id");
+                    if (variantId) {
+                        formData = {
+                            id: variantId,
+                            quantity: quantity
+                        }
+                        addToCart(formData, that.cartInit);
                     }
-                    addToCart(formData, that.cartInit);
-                }
-            })
+                })
+            }
+            // minus cart quantity
+            if (minusCartBiutton) {
+                minusCartBiutton.addEventListener("click", (e) => {
+                    variantId = e.currentTarget.dataset.variant;
+                    let variantQantity = e.currentTarget.dataset.quantity
+                    console.log(variantId,variantQantity,"p");
+                    if (variantId && variantQantity) {
+                        formData = {
+                            id: variantId,
+                            quantity: parseInt(variantQantity) - 1
+                        }
+                        console.log(formData);
+                        cartChange(formData, that.cartInit)
+                    }
+                })
+            }
         })
-        let addCartBiutton = document.querySelectorAll(".cart-plus");
-        let minusCartBiutton = document.querySelector(".cart-minus");
+
         let emptyCart = document.querySelector(".empty-cart")
-        
-        
-
-        // minus cart quantity
-        if (minusCartBiutton) {
-            minusCartBiutton.addEventListener("click", (e) => {
-                variantId = e.currentTarget.dataset.variant;
-                let variantQantity = e.currentTarget.dataset.quantity
-                if (variantId && variantQantity) {
-                    formData = {
-                        id: variantId,
-                        quantity: parseInt(variantQantity) - 1
-                    }
-
-                    cartChange(formData, this.cartInit)
-                }
-            })
-        }
         //empty cart
         if (emptyCart) {
             emptyCart.addEventListener("click", () => {
--- javascript/mobileHeader.js
+++ javascript/mobileHeader.js
@@ -20,6 +20,9 @@ class mobileMenu {
     if( document.querySelectorAll(".mobile-menu-btn.collection").length > 0){
       mobilDrwerButton= document.querySelectorAll(".mobile-menu-btn.collection")
     }
+    if( document.querySelectorAll(".mobile-menu-btn.about").length > 0){
+      mobilDrwerButton= document.querySelectorAll(".mobile-menu-btn.about")
+    }
     // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
     function myFunction() {
       if (window.pageYOffset > sticky) {
